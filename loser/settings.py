b6716da72df4f1e860c898e79c923313a98a72ce
Moiez789 <moiezchaudhary55@gmail.com>
2022-10-08 11:20:58 +0500

    hello

--- loser/settings.py
+++ loser/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn','static_root')
-print (STATIC_DIR)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
